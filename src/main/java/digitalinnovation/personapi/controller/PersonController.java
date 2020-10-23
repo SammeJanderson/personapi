a53143942998fba9771938cc860697bc2f3c5b60
Samme Janderson <samme.san@gmail.com>
2020-10-23 13:24:52 -0300

    developed update feature

--- src/main/java/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/digitalinnovation/personapi/controller/PersonController.java
@@ -39,6 +39,11 @@ public class PersonController {
         return personService.findById(id);
     }
 
+    @PutMapping("/{id}")
+    public MessageResponserDTO updateById(@PathVariable Long id,@RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteByID(@PathVariable Long id) throws PersonNotFoundException {
